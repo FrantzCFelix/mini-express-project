51e724a072f7ed896430652fec2fe839dadc172f
FrantzCFelix <Frantz.C.Felix@gmail.com>
2020-02-08 11:11:16 -0800

    added some route information

--- server.js
+++ server.js
@@ -13,3 +13,22 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // data
+
+// =============================================================
+// Routes
+// =============================================================
+
+// Basic route that sends the user first to the AJAX Page
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'home.html')));
+
+app.get('/table', (req, res) => res.sendFile(path.join(__dirname, 'table.html')));
+
+app.get('/reserve', (req, res) => res.sendFile(path.join(__dirname, 'reserve.html')));
+
+
+// Displays waitlist
+//app.get('/api/waitlist', (req, res) => res.json(waitlist));
+//Displays Tables
+//app.get('/api/waitlist', (req, res) => res.json(tables));
+// Displays a single character or returns false
+
